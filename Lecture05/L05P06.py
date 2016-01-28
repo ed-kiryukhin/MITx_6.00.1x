638a2b2296e0ea9918fa9fb5522ce14efe087b78
Ed Kiryukhin <ed-kiryukhin@users.noreply.github.com>
2016-01-28 13:50:17 +0500

    Create L05P06.py

---
+++ Lecture05/L05P06.py
@@ -0,0 +1,12 @@
+# https://courses.edx.org/courses/course-v1:MITx+6.00.1x_8+1T2016/courseware/sp13_Week_3/videosequence:Lecture_5/
+
+def lenIter(aStr):
+    '''
+    aStr: a string
+    
+    returns: int, the length of aStr
+    '''
+    count = 0
+    for a in aStr:
+        count += 1
+    return count
